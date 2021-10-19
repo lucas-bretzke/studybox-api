ae2c804f92f0eac386926dbb73106b0509e9e823
Lucas-Bretzke <lucasbretzke@gmail.com>
2021-10-19 09:36:10 -0300

    att

--- src/config/database.js
+++ src/config/database.js
@@ -12,5 +12,23 @@ module.exports = {
     define: {
       underscored: true
     }
+  },
+  production: {
+    username: process.env.PROD_DB_USERNAME,
+    password: process.env.PROD_DB_PASSWORD,
+    database: process.env.PROD_DB_DATABASE,
+    host: process.env.PROD_DB_HOST,
+    port: process.env.PROD_DB_PORT,
+    dialect: process.env.PROD_DB_DIALECT,
+    logging: false,
+    define: {
+      underscored: true
+    },
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 };
